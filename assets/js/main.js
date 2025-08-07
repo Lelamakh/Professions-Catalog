68855f1ec719dd19b4db17bd24dc45c25f439b41
Lelamakh <makhokhashvililela@gmail.com>
2025-08-07 19:34:24 -0400

    added slider
    მაინც გავაკეთეთ მე და ჩათიკომ :დდდ

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,30 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 /* END for Contact page */
+
+// mainslider______LM
+const slides = document.querySelectorAll(".lmslide");
+const dots = document.querySelectorAll(".lmdots .lmdot");
+let current = 0;
+
+function updateSlides() {
+  slides.forEach((slide, index) => {
+    slide.classList.toggle("lmslideactive", index === current);
+  });
+
+  dots.forEach((dot, index) => {
+    dot.classList.toggle("lmactivedot", index === current);
+  });
+}
+
+dots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    current = index;
+    updateSlides();
+  });
+});
+
+setInterval(() => {
+  current = (current + 1) % slides.length;
+  updateSlides();
+}, 2000);
