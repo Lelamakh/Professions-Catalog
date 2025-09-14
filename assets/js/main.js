09a03434fe7361550371bd5f639832e13dca92e5
PaataSet <paataseturidze@gmail.com>
2025-09-14 23:12:00 +0400

    add List favorites modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,6 +63,8 @@ setInterval(() => {
 /* mainslider______LM END */
 
 // modal________LM START
+
+/*
 const secondButton = document.querySelector(".lmsecondbutton");
 const modal = document.querySelector(".lmmodaloverlay");
 const quizSection = document.querySelector(".lmquizsection");
@@ -81,6 +83,8 @@ backButton.addEventListener("click", (e) => {
   quizSection.classList.remove("blur");
 });
 
+*/
+
 /* - - - - - - - -
 
 
@@ -90,6 +94,36 @@ START for List page
 
 
 - - - - - - - -*/
+
+/* MODAL START */
+
+// Get the modal
+var PSmodal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  PSmodal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  PSmodal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == PSmodal) {
+    PSmodal.style.display = "none";
+  }
+};
+
+/* MODAL END */
 
 /* ეს კოდი არის ძებნის ველისათვის (დასაწყისი) */
 
