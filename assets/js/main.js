2240eae045ffd60402dde0cb04919251af73d8e4
PaataSet <paataseturidze@gmail.com>
2025-10-05 23:27:04 +0400

    index-PDP mobile version

--- assets/js/main.js
+++ assets/js/main.js
@@ -457,22 +457,29 @@ START for index-PDP page
 
 /* გალერეა END */
 
-var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-var swiper2 = new Swiper(".mySwiper2", {
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  thumbs: {
-    swiper: swiper,
-  },
-});
+// var swiper = new Swiper(".mySwiper", {
+//   spaceBetween: 10,
+//   slidesPerView: 4,
+//   freeMode: true,
+//   watchSlidesProgress: true,
+// });
+// var swiper2 = new Swiper(".mySwiper2", {
+//   spaceBetween: 10,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   thumbs: {
+//     swiper: swiper,
+//   },
+// });
+
+/* ----- Scroll Window START ----- */
+
+//   ???????
+//   ???????
+
+/* ----- Scroll Window START ----- */
 
 /* - - - - - - - -
 
